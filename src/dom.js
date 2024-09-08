bd8a952ecf475581ecb6f7cfc9d3288cbbeea46f
ano373 <andrewamin37@gmail.com>
2024-09-08 16:22:28 +0300

    add event listener for the searchbar

--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,8 @@
 import "./styles.css";
 import logoImg from "../img/weather.png";
-import "./api.js";
+import { constructURL, apiResponse } from "./api.js";
+
+import { degreeUnit } from "./degreeUnit.js";
 const navBarContainer = document.querySelector(".navbar");
 const imgElement = document.querySelector("#logo");
 imgElement.src = logoImg;
@@ -10,6 +12,22 @@ imgElement.height = 100;
 const input = document.querySelector("#searchbar");
 input.setAttribute('type', 'text');
 input.placeholder = "search for a city";
+
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    const city = document.getElementById('searchbar').value;
+    if(city!== ''){
+        const url = constructURL(city,degreeUnit);
+        apiResponse(url).then(json =>{
+            console.log(json);
+        });
+
+    }else{
+        alert('u must provide a city name');
+    }
+    
+    
+});
 
 
 
